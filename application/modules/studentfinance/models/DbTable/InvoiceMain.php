5b20e8185952fef325a9708945d3f66cd664afcb
Agung Sediyono <trisakti_agung06@yahoo.com>
2019-11-13 06:53:55 -0800

    init

--- application/modules/studentfinance/models/DbTable/InvoiceMain.php
+++ application/modules/studentfinance/models/DbTable/InvoiceMain.php
@@ -564,7 +564,7 @@ class Studentfinance_Model_DbTable_InvoiceMain extends Zend_Db_Table_Abstract {
 		$clientid='741';//$program['Client_Id'];
 		if ($process=='createbilling') {
 			if ($re=="1") {
-				 
+				echo strtotime(date('Y-m-d h:s:i'));exit;
 				$bill=100000000+strtotime(date('Y-m-d h:s:i'));
 				echo $bill;exit;
 				$bill=substr($bill, 1,8);
