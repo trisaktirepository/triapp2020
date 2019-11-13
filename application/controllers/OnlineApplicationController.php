f79445d7f56b23266954f8f482d5cc89380d6c7a
Agung Sediyono <trisakti_agung06@yahoo.com>
2019-11-13 01:55:27 -0800

    init

--- application/controllers/OnlineApplicationController.php
+++ application/controllers/OnlineApplicationController.php
@@ -7339,7 +7339,8 @@ class OnlineApplicationController extends Zend_Controller_Action {
 	
 					$this->_redirect($this->view->url(array('module'=>'default','controller'=>'online-application', 'action'=>'viewkartu'),'default',true));
 						
-				} 
+				} else $this->_redirect($this->view->url(array('module'=>'default','controller'=>'applicant-portal', 'action'=>'index'),'default',true));
+					
 			 
 		 
 	}
