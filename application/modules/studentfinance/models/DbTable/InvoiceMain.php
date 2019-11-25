e596189797a0c7e7ff7df5f4811501bd628004ae
Agung Sediyono <trisakti_agung06@yahoo.com>
2019-11-25 05:30:52 -0800

    init

--- application/modules/studentfinance/models/DbTable/InvoiceMain.php
+++ application/modules/studentfinance/models/DbTable/InvoiceMain.php
@@ -809,7 +809,7 @@ class Studentfinance_Model_DbTable_InvoiceMain extends Zend_Db_Table_Abstract {
 	
 		$profil=$dbAppProfile->getData($applid);
 		//exit;
-		//echo var_dump($invoice);exit;
+		echo var_dump($invoice);exit;
 		//$idstd=$invoice['IdStudentRegistration'];
 		//$std=$dbStd->getData($idstd);
 		//$idprogram=$std['IdProgram'];
