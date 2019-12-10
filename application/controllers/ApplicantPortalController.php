8935e3ed8d3cb94f4079319ff83a2de7ac0c3db4
Agung Sediyono <trisakti_agung06@yahoo.com>
2019-12-10 02:23:37 -0800

    init

--- application/controllers/ApplicantPortalController.php
+++ application/controllers/ApplicantPortalController.php
@@ -195,8 +195,9 @@ class ApplicantPortalController extends Zend_Controller_Action
     		//$docData=null;
     		if($docData){
     			if($typeId==30){
+    				
     				$this->_redirect('/online-application/create-usm-card/id/'.$txnId);
-    			}
+    			} else 
     			return DOCUMENT_PATH.DIRECTORY_SEPARATOR.$docData['ad_filepath'].DIRECTORY_SEPARATOR.$docData['ad_filename'];
     		
     		}else{
