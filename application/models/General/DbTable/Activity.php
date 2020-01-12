ad72673c875d0f419f6c7fa3e2c16d00dccea569
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-01-12 12:28:32 -0800

    init

--- application/models/General/DbTable/Activity.php
+++ application/models/General/DbTable/Activity.php
@@ -45,6 +45,7 @@ class App_Model_General_DbTable_Activity extends Zend_Db_Table_Abstract
 		->join(array('b'=>'tbl_activity_calender'),'a.idActivity=b.IdActivity')
 		->join(array('c'=>'tbl_semestermaster'),'c.IdSemesterMaster=b.IdSemesterMain')
 		->where('a.setter="2"')
+		->where('b.IdProgram=?',$idprogram)
 		->where('b.StartDate < CURDATE()');
 		
 		$row = $db->fetchRow($select);
