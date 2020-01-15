c41aa277a198beab8bca36769498e89b5136a6c5
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-01-15 13:05:32 -0800

    init

--- application/models/Finance/DbTable/FeeStructurePlan.php
+++ application/models/Finance/DbTable/FeeStructurePlan.php
@@ -7,7 +7,22 @@ class App_Model_Finance_DbTable_FeeStructurePlan extends Zend_Db_Table_Abstract 
 	protected $_name = 'fee_structure_plan';
 	protected $_primary = "fsp_id";
 		
+	public function getBillingPlanByPackage($fee_structure_id,$paket){
+		$db = Zend_Db_Table::getDefaultAdapter();
+		if ($paket=="A") $paket=0; else $paket=1;
+		$selectData = $db->select()
+		->from(array('fsp'=>$this->_name))
+		->where("fsp.fsp_structure_id = '".$fee_structure_id."'")
+		->where('fsp.fsp_billing_no = '.$paket );
 	
+		$row = $db->fetchRow($selectData);
+	
+		if($row){
+			return $row;
+		}else{
+			return null;
+		}
+	}
 	public function getStructureData($fee_structure_id){
 		$db = Zend_Db_Table::getDefaultAdapter();
 		$selectData = $db->select()
--- application/models/Finance/DbTable/PaymentMain.php
+++ application/models/Finance/DbTable/PaymentMain.php
@@ -89,7 +89,7 @@ class App_Model_Finance_DbTable_PaymentMain extends Zend_Db_Table_Abstract {
 	 * Overite Insert function
 	 */
 	
-	public function insert($data){
+	public function insert(array $data){
 		
 		if( !isset($data['payment_date']) ){
 			$data['payment_date'] = date('Y-m-d H:i:s');
