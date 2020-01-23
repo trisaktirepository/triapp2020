a21edb67cfce4d020db545b4a6f0d28c72bca54e
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-01-23 03:10:45 -0800

    init

--- application/modules/studentfinance/models/DbTable/InvoiceMain.php
+++ application/modules/studentfinance/models/DbTable/InvoiceMain.php
@@ -963,7 +963,7 @@ class Studentfinance_Model_DbTable_InvoiceMain extends Zend_Db_Table_Abstract {
 		->where('im.setter="2"');
 		 
 		$rows = $db->fetchAll($selectData);
-		 
+		echo var_dump($rows);exit;
 		if ($rows) {
 			foreach ($rows as $row) {
 				//cek invoice main
