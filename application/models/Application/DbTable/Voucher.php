39e846fc5ed49d070df8df5685d8dbe68bef6642
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-01-28 22:54:07 -0800

    init

--- application/models/Application/DbTable/Voucher.php
+++ application/models/Application/DbTable/Voucher.php
@@ -33,12 +33,12 @@ class App_Model_Application_DbTable_Voucher extends Zend_Db_Table_Abstract {
 		->from(array('a'=>$this->_name))
 		->where('v_code=?',$voucher)
 		 
-		->where('status <> 1');
+		->where('status is null');
 		
 		if ($telp!=null) $select->where('phone=?',$telp);
 	
 		$row = $db->fetchRow($select);
-			
+		echo $select;exit;
 			
 	
 		return $row;
