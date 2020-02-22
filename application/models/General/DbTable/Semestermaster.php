daed05fb48f8cb9f7ffd353fb7d22c953097047c
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-02-22 03:10:05 -0800

    init

--- application/models/General/DbTable/Semestermaster.php
+++ application/models/General/DbTable/Semestermaster.php
@@ -381,8 +381,8 @@ class App_Model_General_DbTable_Semestermaster extends Zend_Db_Table_Abstract
 			->from(array('det'=>'tbl_activity_calender_intake'))
 			->where('det.IdActivityCalendar=?',$row['id']);
 			$rowdetail=$db->fetchRow($select);
-			echo $select;
-			echo var_dump($rowdetail);exit;
+			//echo $select;
+			//echo var_dump($rowdetail);exit;
 			if ($rowdetail && $intake!=null) {
 				$select = $db->select()
 				->from(array('det'=>'tbl_activity_calender_intake'))
