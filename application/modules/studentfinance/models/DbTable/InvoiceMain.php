8bc4562704e8fae1ff85f02b7748df6c211d8345
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-03-11 10:28:03 -0700

    init

--- application/modules/studentfinance/models/DbTable/InvoiceMain.php
+++ application/modules/studentfinance/models/DbTable/InvoiceMain.php
@@ -739,8 +739,8 @@ class Studentfinance_Model_DbTable_InvoiceMain extends Zend_Db_Table_Abstract {
 				$dbInvoice->update(array("status_remark"=>$respone['message']), "bill_number ='".$invoice['bill_number']."'");
 					
 			}
-			//echo var_dump($invoiceData);
-			//echo var_dump($respone);exit;
+			echo var_dump($invoiceData);
+			echo var_dump($respone);exit;
 		}
 	}
 	public function pushToECollForEnrollment($idinvoice,$dateexprired,$process=null,$mode=null,$re=null) {
