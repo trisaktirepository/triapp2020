805eee9a2a8b7557fcdddfe9ee66823e3a67dbe7
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-03-21 10:49:53 -0700

    init

--- application/modules/studentfinance/models/DbTable/InvoiceMain.php
+++ application/modules/studentfinance/models/DbTable/InvoiceMain.php
@@ -1387,7 +1387,7 @@ class Studentfinance_Model_DbTable_InvoiceMain extends Zend_Db_Table_Abstract {
 		//semester info
 		$semesterDb = new App_Model_General_DbTable_Semestermaster();
 		$semester = $semesterDb->fngetSemestermainDetails($idsemester);
-		//echo var_dump($fee_item); echo $feeitem;exit;
+		echo var_dump($fee_item); echo $feeitem;exit;
 		//echo $feeitem;
 		//filter only selected fee item
 		foreach ($fee_item as $index=>$fee){
@@ -1396,7 +1396,7 @@ class Studentfinance_Model_DbTable_InvoiceMain extends Zend_Db_Table_Abstract {
 			}
 	
 		}
-		echo var_dump($fee_item);exit;
+		
 		if ($allmode=="1") {
 			//get uncalculate payment
 			$recordfee=$dbInvoiceDeatil->getInvoiceDetailByActivity($idsemester, $idRegistration,$idactivity );
