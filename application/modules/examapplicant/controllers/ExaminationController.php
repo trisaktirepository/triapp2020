4404ec22865b0adc37fa9a7836d86e8a3b856ab7
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-03-30 10:28:38 -0700

    init

--- application/modules/examapplicant/controllers/ExaminationController.php
+++ application/modules/examapplicant/controllers/ExaminationController.php
@@ -80,8 +80,7 @@ class Examapplicant_ExaminationController extends Zend_Controller_Action
 	public function startExamAction()
     {
         // action body
-        
-    	 
+    	$this->_helper->layout->setLayout('examapplicant');
     	$trxid=$this->_getParam('idtrx',0);
     	$this->view->title="Examination :";
     	 
@@ -198,62 +197,7 @@ class Examapplicant_ExaminationController extends Zend_Controller_Action
 		
     }
     
- 	public function ajaxSaveStartExamAction($id=null){
-    	
-    	$idSchedule = $this->_getParam('idSche', 0);
-    	$idCourse = $this->_getParam('idCourse', 0);
-    	$idCenter = $this->_getParam('idCenter', 0);
-    	$idStart = $this->_getParam('idStart', 0);
-    	echo $id = $this->_getParam('id', 0);
-    	
-    	
-    	// check is AJAX request or not
-     	/*if (!$this->getRequest() -> isXmlHttpRequest()) {
-        	$this->getResponse() -> setHttpResponseCode(404)
-                              -> sendHeaders();
-         	$this->renderScript('empty.phtml');
-         	return false;
-     	}*/
-    	
-     	if ($this->getRequest()->isXmlHttpRequest()) {
-            $this->_helper->layout->disableLayout();
-        }
-        
-     	$ajaxContext = $this->_helper->getHelper('AjaxContext');
-        $ajaxContext->addActionContext('view', 'html');
-        $ajaxContext->initContext();
-            
-        $time = date('H:i:s');
-        
-		$startDB = new App_Model_Exam_DbTable_StartExam();
-		
-		if($id==1){
-			$data = array(
-		        'idCourse' => $idCourse,
-		        'idSchedule' => $idSchedule,
-		        'idCenter' => $idCenter,
-		        'startTime' => $time
-	        );
-        
-			$idInsert = $startDB->add($data);
-		}else{
-			$dataClose = array(
-		        'endTime' => $time
-	        );
-        	$idInsert = $idStart;
-			$updateData = $startDB->updateData($dataClose,$idStart);
-		}
-
-		$ajaxContext->addActionContext('view', 'html')
-                    ->addActionContext('form', 'html')
-                    ->addActionContext('process', 'json')
-                    ->initContext();
-
-		$json = Zend_Json::encode($idInsert);
-		
-		$this->view->json = $json;
-
-    }
+ 	 
 
     public function ajaxGetQuestionAction($id=null){
     	 
