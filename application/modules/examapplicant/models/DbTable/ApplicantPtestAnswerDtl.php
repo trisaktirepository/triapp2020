4697618c014d6a8dbe3e1bdd7656b9b05df26d91
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-04-01 01:11:41 -0700

    init

--- application/modules/examapplicant/models/DbTable/ApplicantPtestAnswerDtl.php
+++ application/modules/examapplicant/models/DbTable/ApplicantPtestAnswerDtl.php
@@ -61,6 +61,7 @@ class Examapplicant_Model_DbTable_ApplicantPtestAnswerDtl extends Zend_Db_Table_
 	 			->join(array('b'=>'tbl_question_bank'),'a.idQuestion=b.idQuestion')
 	 			->where('b.idQuestion  = '.$row['question_parent']);
 	 			$parent = $db->fetchRow($select);
+	 			echo var_dump($parent);
 	 			$row['question_parent_url']=$parent['question_url'];
 	 		} else $row['question_parent_url']='';
 		return $row;
