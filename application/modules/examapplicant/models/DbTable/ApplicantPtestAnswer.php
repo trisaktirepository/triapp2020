52fc665734af64e4a4444b62674a670db0383b9d
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-04-07 06:25:48 -0700

    init

--- application/modules/examapplicant/models/DbTable/ApplicantPtestAnswer.php
+++ application/modules/examapplicant/models/DbTable/ApplicantPtestAnswer.php
@@ -131,7 +131,7 @@ class Examapplicant_Model_DbTable_ApplicantPtestAnswer extends Zend_Db_Table_Abs
 					->where('a.test_type=?',$postData['test_type']);
 				$set=$db->fetchAll($select);
 				echo var_dump($set);  
-				if ($set) {
+				if (count($set)>1) {
 					echo var_dump($postData['component']); 
 					//get random set according to config
 					$idx=array_rand($set,1);
