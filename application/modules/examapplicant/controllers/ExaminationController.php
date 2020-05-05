e3e6d294363813ae43ed278b70186c30e000a96d
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-05-05 08:19:31 -0700

    init

--- application/modules/examapplicant/controllers/ExaminationController.php
+++ application/modules/examapplicant/controllers/ExaminationController.php
@@ -262,10 +262,10 @@ class Examapplicant_ExaminationController extends Zend_Controller_Action
 	    		foreach ($compprogram as $value) {
 	    			$comprog[]=$value['ac_id'];
 	    		}
-	    		echo var_dump($pstet);
-	    		echo $compcode;exit;
+	    		//echo var_dump($pstet);
+	    		//echo $compcode;exit;
 	    		$component=$dbExamComp->getDataComponent($compcode,$pstet['aph_testtype']);
-	    		 
+	    		echo var_dump($component);exit;
 	    		foreach ($component as $idx=>$comp) {
 	    		
 	    			if (!array_search($comp['ac_id'], $comprog)) {
