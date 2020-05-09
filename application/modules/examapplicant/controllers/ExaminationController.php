40e11ad0d8fb5e3b478be8ca7368d7356e204e21
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-05-09 09:56:12 -0700

    init

--- application/modules/examapplicant/controllers/ExaminationController.php
+++ application/modules/examapplicant/controllers/ExaminationController.php
@@ -18,7 +18,7 @@ class Examapplicant_ExaminationController extends Zend_Controller_Action
     	$appl_id = $auth->getIdentity()->appl_id; 
     	if ($appl_id==202673) {
 			$date="2020-01-19";
-			$time="13:40:00";
+			$time="8:40:00";
 		}
 		else {
 			$date=date('Y-m-d');
@@ -289,7 +289,7 @@ class Examapplicant_ExaminationController extends Zend_Controller_Action
 		$appl_id = $auth->getIdentity()->appl_id;
 		if ($appl_id==202673) {
 			$date="2020-01-19";
-			$time="13:40:00";
+			$time="8:40:00";
 		}
 		else {
 			$date=date('Y-m-d');
