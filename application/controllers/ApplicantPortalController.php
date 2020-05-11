12aba52672303df13b1f1a582f3d3990a0fb33ad
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-05-11 02:25:26 -0700

    init

--- application/controllers/ApplicantPortalController.php
+++ application/controllers/ApplicantPortalController.php
@@ -3417,12 +3417,12 @@ class ApplicantPortalController extends Zend_Controller_Action
     	if($applicant["appl_nationality"]==96){
     		//local
     		$feeStructureDb = new Studentfinance_Model_DbTable_FeeStructure();
-    		$feeStructureData = $feeStructureDb->getApplicantFeeStructure($intakeData['IdIntake'],$program[0]["program_id"],314,$program[0]['IdBranch']);
+    		$feeStructureData = $feeStructureDb->getApplicantFeeStructure($intakeData['IdIntake'],$program[0]["program_id"],314,$program[0]['IdBranchOffer']);
     		$biaya = number_format($biaya, 2, '.', ',');
     	
     	}else{
     		//foreigner
-    		$feeStructureData = $feeStructureDb->getApplicantFeeStructure($intakeData['IdIntake'],$program[0]["program_id"],315,$program[0]['IdBranch']);
+    		$feeStructureData = $feeStructureDb->getApplicantFeeStructure($intakeData['IdIntake'],$program[0]["program_id"],315,$program[0]['IdBranchOffer']);
     		$biaya = $biaya*2;
     		$biaya = number_format($biaya, 2, '.', ',');
     	}
@@ -3435,7 +3435,7 @@ class ApplicantPortalController extends Zend_Controller_Action
     	
     	//fee structure program
     	$feeStructureProgramDb = new Studentfinance_Model_DbTable_FeeStructureProgram();
-    	$feeStructureProgramData = $feeStructureProgramDb->getStructureData($feeStructureData['fs_id'],$program[0]["program_id"],$program[0]['IdBranch']);
+    	$feeStructureProgramData = $feeStructureProgramDb->getStructureData($feeStructureData['fs_id'],$program[0]["program_id"],$program[0]['IdBranchOffer']);
     	
     	//fee structure plan detail
     	$fspdDb = new Studentfinance_Model_DbTable_FeeStructurePlanDetail();
