c2af860299037afbbc697add280ad40395c957df
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-06-07 20:10:00 -0700

    init

--- application/models/Application/DbTable/Pt.php
+++ application/models/Application/DbTable/Pt.php
@@ -73,8 +73,9 @@ class App_Model_Application_DbTable_Pt extends Zend_Db_Table_Abstract {
 			$select->where('TRIM(a.id_wilayah)=?',trim($idwilayah)) ;
 			$select->ORwhere('a.id_wilayah is null') ;
 		}
-		echo $select;
-		return $db->fetchAll($select);
+		$row=$db->fetchAll($select);
+		//echo $select;
+		return $row;
 	
 	}
 	  
