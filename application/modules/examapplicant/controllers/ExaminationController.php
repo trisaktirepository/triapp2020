ab117d9b7c452daad0aebd6934627fbe94649f06
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-06-13 04:45:15 -0700

    init

--- application/modules/examapplicant/controllers/ExaminationController.php
+++ application/modules/examapplicant/controllers/ExaminationController.php
@@ -356,10 +356,10 @@ class Examapplicant_ExaminationController extends Zend_Controller_Action
 	    		if (!$response) {
 	    			 
 	    		 	//get exam script config
-    				echo var_dump($component); exit;
+    				//echo var_dump($component); exit;
 	    			$dbConfig=new Examapplicant_Model_DbTable_ExamScriptConfig();
 	    			$config=$dbConfig->getMatchConfig($currenttest['apt_ptest_code'], $currenttest['apt_aps_id'],$currenttest['app_comp_code']);
-	    			//echo var_dump($config);exit;
+	    			echo var_dump($config);exit;
 	    			if ($config) {
 	    				try {
 	    					$data=array(
