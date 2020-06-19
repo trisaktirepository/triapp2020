8d708e464fae3d0282f3d5a1116f7ed14ec6632c
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-06-19 14:37:19 -0700

    init

--- application/controllers/OnlineApplicationController.php
+++ application/controllers/OnlineApplicationController.php
@@ -2573,7 +2573,7 @@ class OnlineApplicationController extends Zend_Controller_Action {
     	 
     	 $row=$dbSchool->getByWilayah($idwil);
     	  
-    	echo var_dump($row); 
+    	//echo var_dump($row); 
     	$ajaxContext->addActionContext('view', 'html')
     	->addActionContext('form', 'html')
     	->addActionContext('process', 'json')
