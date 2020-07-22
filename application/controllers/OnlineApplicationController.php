c30b336efb8e2d69907fbfef44429cab024e28ce
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-07-21 18:36:16 -0700

    init

--- application/controllers/OnlineApplicationController.php
+++ application/controllers/OnlineApplicationController.php
@@ -9521,7 +9521,7 @@ class OnlineApplicationController extends Zend_Controller_Action {
     		
     		$dbTrasaction=new App_Model_Application_DbTable_ApplicantTransaction();
     		$dbTrasaction->updateData(array('at_intake'=>$formData['intake_id']), $formData['transactionId']);
-    		echo var_dump($formData['subjectcode']);echo $idapply;exit;
+    		
     		if (isset($formData['subjectcode']) && $idapply>0) {
     			$subjectcode=$formData['subjectcode'];
     			$subjectnames=$formData['subjectname'];
