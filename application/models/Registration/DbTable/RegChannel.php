96fdb4aa716157433f86d6a4af17af07730d2cc2
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-07-27 04:47:15 -0700

    init

---
+++ application/models/Registration/DbTable/RegChannel.php
@@ -0,0 +1,61 @@
+<?php
+
+class  App_Model_Registration_DbTable_RegChannel extends Zend_Db_Table_Abstract {
+
+	protected $_name = 'registration_channel';
+	protected $_primary = "id_rc";
+	
+	public function addData($data){		
+	   $id = $this->insert($data);
+	   return $id;
+	}
+	
+	public function updateData($data,$id){
+		 $this->update($data, $this->_primary .' = '. (int)$id);
+	}
+	
+	public function deleteData($id){		
+	  $this->delete($this->_primary .' =' . (int)$id);
+	}
+	
+	 
+	
+	public function getData($id){
+	
+		$db = Zend_Db_Table::getDefaultAdapter();
+	
+		$select = $db ->select()
+		->from(array('a'=>$this->_name))
+		->where("id_rc = ?",$id);
+		$row = $db->fetchRow($select);
+		return $row;
+	}
+	
+	 
+	public function isInRegistration($idstd,$rsid){
+	
+		$db = Zend_Db_Table::getDefaultAdapter();
+	
+		$select = $db ->select()
+		->from(array('a'=>$this->_name))
+		->where("a.transaction_id = ?",$idstd)
+		->where("a.rsd_id=?",$rsid);
+		$row = $db->fetchRow($select);
+		return $row;
+	}
+	
+	public function getDataByTrx($idstd){
+	
+		$db = Zend_Db_Table::getDefaultAdapter();
+	
+		$select = $db ->select()
+		->from(array('a'=>$this->_name))
+		->where("a.transaction_id = ?",$idstd)
+		->where("a.rsd_id is not null");
+		$row = $db->fetchRow($select);
+		return $row;
+	}
+	
+}
+
+?>
