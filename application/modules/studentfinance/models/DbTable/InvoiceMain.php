81fcc1e1bd09953284501b886b57c8c042cd3878
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-08-18 10:58:26 -0700

    init

--- application/modules/studentfinance/models/DbTable/InvoiceMain.php
+++ application/modules/studentfinance/models/DbTable/InvoiceMain.php
@@ -1007,7 +1007,7 @@ class Studentfinance_Model_DbTable_InvoiceMain extends Zend_Db_Table_Abstract {
 		 
 		$rows = $db->fetchAll($selectData);
 		//echo $selectData;
-		echo var_dump($rows);  exit;
+		//echo var_dump($rows);  exit;
 		if ($rows) {
 			$status="0";
 			foreach ($rows as $row) {
@@ -1015,7 +1015,7 @@ class Studentfinance_Model_DbTable_InvoiceMain extends Zend_Db_Table_Abstract {
 				 
 				$dbBundle=new Studentfinance_Model_DbTable_BundleFee();
 				$bundle=$dbBundle->getCurrentSetup(1, $std['IdCollege'], $std['IdProgram'], $std['IdBranch'], $row['IdSemesterMain'], $row['idActivity'],$std['IdProgramMajoring']);
-				//echo var_dump($bundle);exit;
+				echo var_dump($bundle);exit;
 				if ($bundle) {
 					if (($row['idActivity']==39 || $row['idActivity']==40 || $row['idActivity']==42)) {
 						
