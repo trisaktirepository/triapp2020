2e846d9d79ee6d6f3397e2d1c9257f8d86e670c8
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-09-06 22:32:46 -0700

    init

--- application/controllers/StudentPortalController.php
+++ application/controllers/StudentPortalController.php
@@ -3839,7 +3839,7 @@ class StudentPortalController extends Zend_Controller_Action
 			$ref=$dbReflection->getDataStd($cgaid, $registration_id);
 			$att[$key]['capability']=$ref['capability'];
 			$att[$key]['uncapability']=$ref['uncapability'];
-			$att[$key]['FullName']=$dbStaff->getStaffFullName($value['IdLecturer']);
+			$att[$key]['FullName']=$dbStaff->getStaffFullName($value['lecturer_id']);
 		}
 		$this->view->attendance=$att;
 		$grp=$dbCourse->getInfo($grpid);
