07bab750a159189b95e36efa5dcd4eeeb5703a80
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-10-01 22:39:48 -0700

    init

--- application/controllers/ExtraActivityController.php
+++ application/controllers/ExtraActivityController.php
@@ -86,10 +86,10 @@ M/iqHGl3h765f2buMoXbaRAnYqAk6W3XF5QtMIs2o97oi7HMM3/gVeKxZZQtGySr
     	//$data='{"NIM":'.$nim.';"TOKEN:"'.$token.';"OTP":'.$pin.'}';
     	$res = openssl_public_encrypt($data,$encypteddata,$this->_publickey,OPENSSL_PKCS1_PADDING);
     	if ($res)	{ 
-	    	$data = $encypteddata.'&apikey="'.$this->_apikey.'"';
+	    	$data = $encypteddata.'&apikey='.$this->_apikey;
 	    	echo $data;echo '<br>';
 	    	return $data;
-    	} else return '';
+    	} else return '--';
     	
     }
     
