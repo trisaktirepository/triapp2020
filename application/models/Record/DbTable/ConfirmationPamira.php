d18e4104d611534a8d138dc726de29e8ab0ff74c
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-10-02 06:03:17 -0700

    init

--- application/models/Record/DbTable/ConfirmationPamira.php
+++ application/models/Record/DbTable/ConfirmationPamira.php
@@ -9,9 +9,7 @@ class App_Model_Record_DbTable_ConfirmationPamira extends Zend_Db_Table_Abstract
     	$auth = Zend_Auth::getInstance();
     	if ($auth->getIdentity()->role==$role && $auth->getIdentity()->registration_id==$registration_id) {
     		$redirector = Zend_Controller_Action_HelperBroker::getStaticHelper('Redirector');
-    			
-    		
-    		$redirector->gotoUrl('/extra-activity/pemira');
+     		$redirector->gotoUrl('/extra-activity/pemira');
     	}
     		 
    	}
