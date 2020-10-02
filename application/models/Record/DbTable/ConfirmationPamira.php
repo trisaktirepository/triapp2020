ecfa8194db742d3cef062a5ee3dbd4502c089a23
Agung Sediyono <trisakti_agung06@yahoo.com>
2020-10-02 06:11:57 -0700

    init

--- application/models/Record/DbTable/ConfirmationPamira.php
+++ application/models/Record/DbTable/ConfirmationPamira.php
@@ -26,7 +26,7 @@ class App_Model_Record_DbTable_ConfirmationPamira extends Zend_Db_Table_Abstract
    			$db = Zend_Db_Table::getDefaultAdapter();
    			$select = $db->select()
    			->from(array($this->_name)) 
-   			->where('a.IdStudentRegistration=?',$idstd);
+   			->where('IdStudentRegistration=?',$idstd);
    			$row=$db->fetchAll($select);
    			if (count($row)<=3)
    				return true;
