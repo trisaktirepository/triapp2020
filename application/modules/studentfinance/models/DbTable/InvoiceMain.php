d37c114ffe1bf9278a9417f034d069e41eff6c28
Agung Sediyono <trisakti_agung06@yahoo.com>
2021-01-18 17:39:58 -0800

    init

--- application/modules/studentfinance/models/DbTable/InvoiceMain.php
+++ application/modules/studentfinance/models/DbTable/InvoiceMain.php
@@ -1243,6 +1243,7 @@ class Studentfinance_Model_DbTable_InvoiceMain extends Zend_Db_Table_Abstract {
 														
 														//$dbtxt->add(array('txt'=>$actualamount.'='.$itemss.' '.$idstd));
 														echo $actualamount;echo '-';echo $itemss;echo '-'.$row['idActivity'];echo '<br>';  
+														exit;
 														if ($actualamount-$itemss!=0) return $row['idActivity'];
 														
 														//echo $status;echo '<br>';
