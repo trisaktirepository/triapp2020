da1f871e8e4f49bb541dd2f96ba4433e9eff64d3
Agung Sediyono <trisakti_agung06@yahoo.com>
2021-01-21 15:37:28 -0800

    init

--- application/modules/studentfinance/models/DbTable/InvoiceMain.php
+++ application/modules/studentfinance/models/DbTable/InvoiceMain.php
@@ -1360,6 +1360,7 @@ class Studentfinance_Model_DbTable_InvoiceMain extends Zend_Db_Table_Abstract {
 									foreach ($value['bundledetail'] as $det) {
 										$totalamountact=$totalamountact+$det['fee']['amount'];
 										if (isset($det['discount'])) foreach ($det['discount'] as $disc) $discount=$discount-$disc['amount'];
+										echo var_dump($det);echo '<br>';
 										//$totalamountact=$totalamountact+$det['fee']['amount'];
 										if (abs($discount)>0 && isset($det['fee'][0]['fi_name_bahasa'])) {
 											$restamount[$det['fee'][0]['fi_id']]['amount']=$discount;
@@ -1368,8 +1369,8 @@ class Studentfinance_Model_DbTable_InvoiceMain extends Zend_Db_Table_Abstract {
 									}
 								}
 								//echo var_dump($act);
-								//echo $totalamount.'='.$totalamountact;
-								//exit;
+								echo $totalamount.'='.$totalamountact;
+								exit;
 								if ($totalamount!=($totalamountact+$discount)) 
 									return $row['idActivity'];
 							}
