ef86b51dda5182d1ae29e651659731d05c683615
Agung Sediyono <trisakti_agung06@yahoo.com>
2021-01-23 23:37:18 -0800

    init

--- application/modules/zoom/controllers/NotificationController.php
+++ application/modules/zoom/controllers/NotificationController.php
@@ -6,7 +6,15 @@ class Zoom_NotificationController extends Zend_Controller_Action {
 	public function meetingAction() {
 		
 		$this->_helper->layout->setLayout('application');
-		 
+
+		if ($this->getRequest()->isXmlHttpRequest()) {
+			$this->_helper->layout->disableLayout();
+		}
+		
+		$ajaxContext = $this->_helper->getHelper('AjaxContext');
+		$ajaxContext->addActionContext('view', 'html');
+		$ajaxContext->initContext();
+		
     	$registry = Zend_Registry::getInstance();
 		$locale = $registry->get('Zend_Locale');
 	 	$dbZoom=new Zoom_Model_DbTable_Notification();
@@ -14,8 +22,22 @@ class Zoom_NotificationController extends Zend_Controller_Action {
     	if ($this->getRequest()->isPost()) {
     		$formData = $this->getRequest()->getPost();
     		$dbZoom->add(var_dump($formData));
-    		return 200;
+    		 
     	}
+    	
+    	$ajaxContext = $this->_helper->getHelper('AjaxContext');
+    	$ajaxContext->addActionContext('view', 'html');
+    	$ajaxContext->initContext();
+    	
+    	$ajaxContext->addActionContext('view', 'html')
+    	->addActionContext('form', 'html')
+    	->addActionContext('process', 'json')
+    	->initContext();
+    	
+    	$json = Zend_Json::encode(array('200'));
+    	
+    	echo $json;
+    	exit();
     	 
 	}
 }
