a42f2cc0f8881ff789ae101604de847f32030530
Agung Sediyono <trisakti_agung06@yahoo.com>
2021-01-27 15:00:15 -0800

    init

--- application/modules/studentfinance/controllers/InvoiceController.php
+++ application/modules/studentfinance/controllers/InvoiceController.php
@@ -696,10 +696,10 @@ class Studentfinance_InvoiceController extends Zend_Controller_Action {
 										if (!$dbDiscountSetup->isStudentApplied($maind,$registration['IdStudentRegistration'])) $validstd="0";
 										 
 									}
-								// echo $maind.'=';echo $validsem;echo $validlevel;echo $validintake;echo $validstd; echo '<br>';
+								echo $maind.'=';echo $validsem;echo $validlevel;echo $validintake;echo $validstd; echo '<br>';
 								if (!($validsem=="1" && $validlevel=="1" && $validintake=="1" && $validstd=="1")) unset($discounttype[$idx]);
 							}
-							//echo var_dump($discounttype);exit;
+							echo var_dump($discounttype);exit;
 							if ($discounttype) {
 								foreach ($discounttype as $idx=>$value) {
 									$setup=$value['discount'];
