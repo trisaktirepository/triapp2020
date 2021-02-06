3453eda710063ef67b6babafcad76404707a7d94
Agung Sediyono <trisakti_agung06@yahoo.com>
2021-02-06 14:22:31 -0800

    init

--- application/modules/studentfinance/models/DbTable/InvoiceMain.php
+++ application/modules/studentfinance/models/DbTable/InvoiceMain.php
@@ -1440,6 +1440,7 @@ class Studentfinance_Model_DbTable_InvoiceMain extends Zend_Db_Table_Abstract {
 										//if ($row['idActivity']==44) {
 										echo $totalamount.'='.$totalamountact.'='.$discitem;  exit;
 										 
+										
 										//}
 										//echo '<br>';
 										
